6a834a9af7fb564421742b42d28b64c43f0c5381
BaimoQilin <admin@baimoqilin.top>
2026-05-23 19:54:32 +0800

    chore: bump version to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.baimoqilin.aether"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
